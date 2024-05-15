cbbfe5560ab71cef22bb0e9cfd64650cad7ae1d1
sergeytashkinov <tashkinov2@gmail.com>
2024-05-15 09:29:12 +0300

    new func substrac

--- funcs.py
+++ funcs.py
@@ -1,3 +1,7 @@
 def add(a: float, b: float) -> float:
     '''Функция принимает на вход 2 числа и возвращает их сумму'''
     return a + b
+
+def subtrac(a: float, b: float) -> float:
+    '''Функция принимает на вход 2 числа и возвращает их разницу'''
+    return a - b
